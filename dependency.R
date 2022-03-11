2fd09594aa55accf59df2a6f77c6abdeaf2ed321
Shivani Srivastava <shivani141296@gmail.com>
2022-03-11 09:53:35 +0530

    Create dependency.R

---
+++ dependency.R
@@ -0,0 +1,3 @@
+  if (!require('shiny')){install.packages("shiny")}; library("shiny")
+  if (!require('CausalImpact')){install.packages("CausalImpact")}; library("CausalImpact")
+  if (!require('data.table')){install.packages("data.table")}; library("data.table")
